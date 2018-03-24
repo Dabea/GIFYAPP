ff60260a00242d1a802337dc9e5385bd34f43b04
Abraham <abeis@msn.com>
2018-03-24 16:21:52 -0500

    completed display functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
-let tagCollection = ['Animials', 'Pop' , 'Music', 'Cats'];
+"use strict";
+let tagCollection = ['Animials', 'Pop' , 'Music', 'Cats' , 'rain'];
 
 /**
  * Generates the tags and appends it to the page
@@ -15,12 +16,12 @@ function generateTags(name){
 function buildView(){
     for(let i = 0; i < tagCollection.length; i++){
         generateTags(tagCollection[i]);
-       
     }
 }
 
 $('.banner').on('click', '.tag', function(event){
-    const apiKey = 'Iphjhlb4smlWWHFJOBbepwW6q4Q5V71H'
+    event.preventDefault();
+    const apiKey = 'Iphjhlb4smlWWHFJOBbepwW6q4Q5V71H';
     const serch = $(event.target).data('tag');
     const limit = 10;
     const builtUrl = `https://api.giphy.com/v1/gifs/search?q=${serch}&api_key=${apiKey}&limit=${limit}`;
@@ -28,12 +29,17 @@ $('.banner').on('click', '.tag', function(event){
         url: builtUrl,
         method: 'GET'
     }).then(function(data){
+        $('.img-area').empty();
         for(let i = 0; i < data.data.length; i++){
-            $('.img-area').append(`<img width="300"  src='${data.data[i].images.original_still.url}'>`);
+            $('.img-area').append(` <div class="img-container" ><img style="width:100%" data-loop="${data.data[i].images.original.url}" data-static="${data.data[i].images.original_static.url}" src='${data.data[i].images.original.url}'> <span> Rateing:${data.data[i].rating}</span</div>`);
         }
-       
-    })  
-})
+    });
+});
+
+$('.banner').on('click', '.delete-symbol', function(event){
+    event.stopImmediatePropagation();
+    $(event.target).closest('.tag').remove();
+});
 
 
 
